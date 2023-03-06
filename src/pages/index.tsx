0e5c2cbb707effbcce016ac99f06564ef68ad57e
John Rodriguez <43676480+jismah@users.noreply.github.com>
2023-03-06 16:40:43 -0400

    Changes UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,5 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-import prisma from '../../lib/prisma';
-import { GetStaticProps } from 'next';
-
-export const getStaticProps: GetStaticProps = async () => {
-  const listStudents = await prisma.student.findMany();
-
-  return {
-    props: { listStudents },
-    revalidate: 10
-  }
-}
-
-
-// async function main() {
-//   const students = await prisma.student.findMany()
-//   console.log(students);
-// }
-
 
 export default function Home() {
   return (
