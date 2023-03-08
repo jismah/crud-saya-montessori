b35d7343ff1153ed19bb52e6493d2434b5fb5d3e
John Rodriguez <43676480+jismah@users.noreply.github.com>
2023-03-08 14:45:33 -0400

    student View Create

---
+++ src/pages/students/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import React, { Component } from 'react';
+
+const Student: NextPage = () => {
+    return (
+        <>
+            <h1>Student</h1>
+        </>
+    )
+}
+
+export default Student;
